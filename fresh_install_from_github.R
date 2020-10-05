66e2b392a06d686548d077934cee9c5678e83dd5
pecanka <62954638+pecanka@users.noreply.github.com>
2020-10-05 17:43:46 +0200

    Updates

--- fresh_install_from_github.R
+++ fresh_install_from_github.R
@@ -1,5 +1,4 @@
-url_build = 'https://www.github.com/pecanka/utilbox/raw/master/build/utilbox_0.1.0.0.tar.gz'
-install.packages(url_build, repos = NULL, type = "source")
+install.packages('https://www.github.com/pecanka/utilbox/raw/master/build/utilbox_0.1.0.0.tar.gz', repos=NULL, type="source")
 
 
 
